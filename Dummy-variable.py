8c522aeb6e02e91cf10bd2581d6f607d4438aa27
vipulkumarsahu18-coder <vipulkumarsahu565@gmail.com>
2026-04-22 10:29:54 +0530

    Add dummy variable creation using pandas

--- Dummy-variable.py
+++ Dummy-variable.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+data = {
+    "Name": ["A", "B", "C", "D"],
+    "City": ["Delhi", "Mumbai", "Delhi", "Chennai"],
+    "Gender": ["Male", "Female", "Female", "Male"]
+}
+
+df = pd.DataFrame(data)
+
+dummies = pd.get_dummies(df, columns=["City", "Gender"])
+
+print(dummies)
